9606526a86122e69ff8636d58643f7bfb7f03b1d
Misbah <hafizamisbah165@gmail.com>
2025-08-11 02:59:27 +0500

    fiz soem issuus

--- src/constants/SampleImages.js
+++ src/constants/SampleImages.js
@@ -1,7 +1,7 @@
 // sampleImages.js
 import img1 from "../assets/images/photos1.png";
 import img2 from "../assets/images/photos2.png";
-import img3 from "../assets/images/photos3.png";
+import img3 from "../assets/images/Photos3.png";
 
 const baseImages = [img1, img2, img3];
 
