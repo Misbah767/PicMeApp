509154675922aab822a74e1b69c3bf5b35d796bb
Misbah <hafizamisbah165@gmail.com>
2025-08-11 10:57:51 +0500

    fix case sensitive for deploy

--- src/screens/PGScreen/PGLContent.jsx
+++ src/screens/PGScreen/PGLContent.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./PGLContent.css";
+import "./PglContent.css";
 import FindInput from "../../Components/FindInput/FindInput";
 import SearchInput from "../../Components/SearchInput/SearchInput";
 import LeftArrowIcon from "../../assets/icons/LeftArrowIcon";
